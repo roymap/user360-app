b8fbdfeead1f9ddb2e459301f32f9cf4bb40876a
Roy Pereira <roymap67@gmail.com>
2022-05-18 22:54:46 -0400

    forgot to add the /dist dir

---
+++ dist/spa/assets/head.50af1d79.js
@@ -0,0 +1,1 @@
+import{b as e}from"./index.93aafbe4.js";import{c as t}from"./index.19959350.js";var d=e(({app:o})=>{console.log("boot.head");const a=t();o.use(a)});export{d as default};
---
+++ dist/spa/assets/pinia.e1594737.js
@@ -0,0 +1,1 @@
+import{b as a,c as i}from"./index.93aafbe4.js";var t=a(({app:o})=>{console.log("boot.pinia"),o.use(i())});export{t as default};
