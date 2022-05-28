0b309ab38c7380e13c7047814ae2d981dbf50ae7
Roy Pereira <roymap@users.noreply.github.com>
2022-05-28 00:08:14 -0400

    Delete dist/spa directory

--- dist/spa/assets/head.ce7b758b.js
+++
@@ -1,1 +0,0 @@
-import{b as o}from"./index.9f6bd162.js";import{c as r}from"./index.3bb66fa0.js";var d=o(({app:a})=>{const e=r();a.use(e)});export{d as default};
--- dist/spa/assets/use-quasar.0b834d35.js
+++
@@ -1,1 +0,0 @@
-import{i as a,q as r}from"./index.9f6bd162.js";function u(){return a(r)}export{u};
